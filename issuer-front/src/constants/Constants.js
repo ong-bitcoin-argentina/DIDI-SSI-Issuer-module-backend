1ec4d6cbafa9074264156db100ad7dff75d9b419
Daniel Obon <daniel.obon@creativecoefficient.net>
2019-12-02 17:56:25 -0300

    rename env var

--- issuer-front/src/constants/Constants.js
+++ issuer-front/src/constants/Constants.js
@@ -1,4 +1,4 @@
-const API = process.env.API_URL || "http://localhost:3500/api/1.0/didi_issuer";
+const API = process.env.REACT_APP_API_URL || "http://localhost:3500/api/1.0/didi_issuer";
 
 module.exports = {
 	API_ROUTES: {
