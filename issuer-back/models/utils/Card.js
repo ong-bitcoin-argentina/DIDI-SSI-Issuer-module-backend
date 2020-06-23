6603040afb9d378ac98c328f810aa5a6c32358f5
Nicolas Laitan <nlaitan@atixlabs.com>
2020-06-23 17:20:50 -0300

    fix: vuelvo a usar full_url

--- issuer-back/models/utils/Card.js
+++ issuer-back/models/utils/Card.js
@@ -1,6 +1,6 @@
-const { ADDRESS } = require("../../constants/Constants");
+const { FULL_URL } = require("../../constants/Constants");
 
-const sancorImgUrl = `${ADDRESS}/img/CredencialSancor.png`;
+const sancorImgUrl = `${FULL_URL}/img/CredencialSancor.png`;
 
 const defaultRows = [{ columns: 2 }, { columns: 1 }, { columns: 1 }, { columns: 1 }];
 
