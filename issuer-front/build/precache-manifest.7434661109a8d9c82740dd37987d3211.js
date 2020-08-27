58823bb3fac5ac11abf20fb28e184ed5beb10a42
agus <atroiano@atixlabs.com>
2020-08-27 17:37:50 -0300

    feat: add new general styling on Login Page and Main Layouts.

---
+++ issuer-front/build/precache-manifest.7434661109a8d9c82740dd37987d3211.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4e9dadc3682411906cdbf2ecc95286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2bc45ebfc7f79a0263f",
+    "url": "/static/css/2.c110b1a9.chunk.css"
+  },
+  {
+    "revision": "1192dfb646d2a1744052",
+    "url": "/static/css/main.463dabe4.chunk.css"
+  },
+  {
+    "revision": "b2bc45ebfc7f79a0263f",
+    "url": "/static/js/2.be175244.chunk.js"
+  },
+  {
+    "revision": "7b4c3933355157258d7f31c9a03ce3fd",
+    "url": "/static/js/2.be175244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1192dfb646d2a1744052",
+    "url": "/static/js/main.6fed498f.chunk.js"
+  },
+  {
+    "revision": "f04ccae57bbb503d4c9f",
+    "url": "/static/js/runtime-main.fea047c2.js"
+  },
+  {
+    "revision": "6df6277c239fc2c31917373015120129",
+    "url": "/static/media/1.6df6277c.png"
+  },
+  {
+    "revision": "5751f83de932fe26523d6de5be3b9916",
+    "url": "/static/media/2.5751f83d.png"
+  },
+  {
+    "revision": "6b7baf8373ae8d970fd0df1813288bd6",
+    "url": "/static/media/3.6b7baf83.png"
+  }
+]);
