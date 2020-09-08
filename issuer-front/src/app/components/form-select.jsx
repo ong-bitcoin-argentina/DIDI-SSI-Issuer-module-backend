f1393d09de87fbda6db8283a18e4a94eb345ac88
Nicolas Laitan <nlaitan@atixlabs.com>
2020-09-08 10:30:15 -0300

    fix: remove labeled prop

--- issuer-front/src/app/components/form-select.jsx
+++ issuer-front/src/app/components/form-select.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FormControl, Select, InputLabel, MenuItem } from "@material-ui/core";
 
-export default ({ value, label, onChange, variant = "outlined", labeled = true, list = [] }) => {
+export default ({ value, label, onChange, variant = "outlined", list = [] }) => {
 	return (
 		<FormControl variant={variant} style={{ width: "100%" }}>
-			{labeled && <InputLabel id="outlined-label">{label}</InputLabel>}
+			{!!label && <InputLabel id="outlined-label">{label}</InputLabel>}
 			<Select labelId="outlined-label" value={value} onChange={onChange} label={label}>
 				{list.map((item, index) => (
 					<MenuItem value={item.value} key={index}>
