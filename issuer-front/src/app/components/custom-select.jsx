9da2c474e9cffac169b8484fc2eb40ddbaf141ee
Nicolas Laitan <nlaitan@atixlabs.com>
2020-09-08 10:20:46 -0300

    fix: change name to formselect

--- issuer-front/src/app/components/custom-select.jsx
+++ issuer-front/src/app/components/custom-select.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Select, MenuItem } from "@material-ui/core";
 
 const CustomSelect = ({ onChange, options, label, field }) => {
--- issuer-front/src/app/components/form-select.jsx
+++ issuer-front/src/app/components/form-select.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FormControl, Select, InputLabel, MenuItem } from "@material-ui/core";
 
-export default ({ value, label, onChange, list = [] }) => {
+export default ({ value, label, onChange, variant = "outlined", labeled = true, list = [] }) => {
 	return (
-		<FormControl variant="outlined" style={{ width: "100%" }}>
-			<InputLabel id="outlined-label">{label}</InputLabel>
+		<FormControl variant={variant} style={{ width: "100%" }}>
+			{labeled && <InputLabel id="outlined-label">{label}</InputLabel>}
 			<Select labelId="outlined-label" value={value} onChange={onChange} label={label}>
 				{list.map((item, index) => (
 					<MenuItem value={item.value} key={index}>
