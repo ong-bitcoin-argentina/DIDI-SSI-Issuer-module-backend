d3504164d6fee78e0c1833e4311c454ef2f33011
Uriel Quevedo <uquevedo@atixlabs.com>
2021-02-11 14:29:17 -0300

    feat: helper text in create user modal

--- issuer-front/src/app/components/CreateUserModal.jsx
+++ issuer-front/src/app/components/CreateUserModal.jsx
@@ -41,6 +41,7 @@ const CreateUserModal = ({ open, close, onSubmit, userData, title, required, pro
 			name: "password",
 			placeholder: "Contraseña",
 			type: showPassword ? "text" : "password",
+			helperText: "Requiere al menos 6 caracteres",
 			required
 		},
 		{
@@ -110,18 +111,16 @@ const CreateUserModal = ({ open, close, onSubmit, userData, title, required, pro
 				<DialogContent style={{ margin: "0px 0 25px" }}>
 					<Grid container item xs={12} justify="center">
 						<Grid item xs={9}>
-							{INPUTS.map(({ name, placeholder, type, required }, index) => (
+							{INPUTS.map(({ name, placeholder, ...rest }, index) => (
 								<TextField
 									key={index}
 									style={{ marginBottom: "25px" }}
-									id={`"user-${name}-input"`}
 									label={placeholder}
-									name={name}
-									type={type}
 									onChange={handleChange}
 									defaultValue={newUser[name]}
-									required={required}
+									name={name}
 									fullWidth
+									{...rest}
 									InputProps={{
 										endAdornment: name === "password" && (
 											<IconButton style={{ padding: 0 }} onClick={() => setShowPassword(pw => !pw)}>
