4c8ec944c04b5370f1f47abe6bcd8f9cda650356
Uriel Quevedo <uquevedo@atixlabs.com>
2021-02-23 10:01:25 -0300

    feat: generic style

--- issuer-front/src/app/components/TabDescription/TabDescription.jsx
+++ issuer-front/src/app/components/TabDescription/TabDescription.jsx
@@ -6,7 +6,7 @@ import "./_style.scss";
 const TabDescription = ({ tabName }) => {
 	return (
 		<Grid container xs={12} direction="column" className="tab-description">
-			<h1 className="title">{TAB_TEXT[tabName].TITLE}</h1>
+			<h1 className="m-0 p-0">{TAB_TEXT[tabName].TITLE}</h1>
 			<p>{TAB_TEXT[tabName].DESCRIPTION}</p>
 		</Grid>
 	);
