89f72ddc79ad1ded0721e8a0c3e035398feec7ce
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2021-04-16 19:28:40 -0300

    Remove front code

--- issuer-front/src/constants/Requests.js
+++
@@ -1,8 +0,0 @@
-export const options = (token, method = "GET", data) => ({
-	method,
-	headers: {
-		"Content-Type": "application/json",
-		token
-	},
-	...(method !== "GET" && { body: JSON.stringify(data) })
-});
