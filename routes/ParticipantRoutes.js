0c1eb74d1c329fd0df5e5b6f8e31de9cca9cae3f
Diegoro21 <diero17@gmail.com>
2021-04-30 15:51:32 -0300

    Documentacion endpoints ParticipantRoutes MM-285

--- routes/ParticipantRoutes.js
+++ routes/ParticipantRoutes.js
@@ -8,8 +8,25 @@ const Constants = require("../constants/Constants");
 const Messages = require("../constants/Messages");
 
 /**
- *	Retorna los dids y nombres de todos los participantes conocidos
+ * @openapi
+ *   /participant/dids:
+ *   get:
+ *     summary: Retorna los dids y nombres de todos los participantes conocidos.
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.get(
 	"/dids",
 	Validator.validate([
@@ -34,9 +51,26 @@ router.get(
 );
 
 /**
- *	Retorna los participantes con informacion no vinculada a un modelo de certificado en particular
- *	e indica que tipo de informacion posee (cual de los certificados)
+ * @openapi
+ *   /participant/global:
+ *   get:
+ *     summary: Retorna los participantes con informacion no vinculada a un modelo de certificado en particular.
+ *     description: Indica que tipo de informacion posee (cual de los certificados).
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.get(
 	"/global",
 	Validator.validate([
@@ -91,8 +125,30 @@ router.get(
 );
 
 /**
- *	Retorna los participantes con informacion vinculada a un modelo de certificado
+ * @openapi
+ *   /participant/all/:{templateId}:
+ *   get:
+ *     summary: Retorna los participantes con informacion vinculada a un modelo de certificado.
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - name: templateId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.get(
 	"/all/:templateId",
 	Validator.validate([
@@ -118,9 +174,30 @@ router.get(
 );
 
 /**
- *	Retorna la info del el participante con el codigo indicado, si la data de este fue modificada
- *	(para hacer pulling en qr)
+ * @openapi
+ *   /participant/new/:{requestCode}:
+ *   get:
+ *     summary: Retorna la informacion del participante con el codigo indicado, si la data fue modificada.
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - name: requestCode
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.get(
 	"/new/:requestCode",
 	Validator.validate([
@@ -143,8 +220,30 @@ router.get(
 );
 
 /**
- * 	Retorna la info de participante asociada a un usuario en particular
+ * @openapi
+ *   /participant/:{did}:
+ *   get:
+ *     summary: Retorna la info de participante asociada a un usuario en particular.
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - name: did
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.get(
 	"/:did",
 	Validator.validate([
@@ -167,9 +266,69 @@ router.get(
 );
 
 /**
- *	 Inicializa la data de participante con unicamente el did y nombre
- *	 (carga por csv)
+ * @openapi
+ *   /participant/new/:
+ *   post:
+ *     summary: Inicializa la data de participante con unicamente el did y nombre.
+ *     description: Carga por csv.
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *     requestBody:
+ *       required:
+ *         - data
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               data:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   required:
+ *                     - "name"
+ *                     - "did"
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                     did:
+ *                       type: string
+ *                     data:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         required:
+ *                           - "name"
+ *                           - "value"
+ *                         properties:
+ *                             name:
+ *                               type: string
+ *                             value:
+ *                               type: string
+ *                     templateId:
+ *                       type: string
+ *                     requestCode:
+ *                       type: string
+ *                     new:
+ *                       type: boolean
+ *                     deleted:
+ *                       type: boolean
+ *                     createdOn:
+ *                       type: string
+ *                       format: date-time
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.post(
 	"/new/",
 	Validator.validate([
@@ -201,8 +360,79 @@ router.post(
 );
 
 /**
- *	Modifica la data de participante con los datos recibidos
+ * @openapi
+ *   /participant/:{id}/:
+ *   put:
+ *     summary: Modifica la data de participante con los datos recibidos.
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     requestBody:
+ *       required:
+ *         - name
+ *         - templateId
+ *         - data
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               templateId:
+ *                 type: string
+ *               data:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   required:
+ *                     - "name"
+ *                     - "did"
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                     did:
+ *                       type: string
+ *                     data:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         required:
+ *                           - "name"
+ *                           - "value"
+ *                         properties:
+ *                             name:
+ *                               type: string
+ *                             value:
+ *                               type: string
+ *                     templateId:
+ *                       type: string
+ *                     requestCode:
+ *                       type: string
+ *                     new:
+ *                       type: boolean
+ *                     deleted:
+ *                       type: boolean
+ *                     createdOn:
+ *                       type: string
+ *                       format: date-time
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.put(
 	"/:id/",
 	Validator.validate([
@@ -235,8 +465,30 @@ router.put(
 );
 
 /**
- *	Marca la data de participante como borrada
+ * @openapi
+ *   /participant/:{id}:
+ *   delete:
+ *     summary: Marca la data de participante como borrada.
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.delete(
 	"/:id",
 	Validator.validate([
@@ -262,8 +514,35 @@ router.delete(
 // -- disclosure requests --
 
 /**
- *	Carga de info de participante global a partir de un pedido de certificado realizado con "/template/request/:requestCode"
+ * @openapi
+ *   /participant/:{requestCode}:
+ *   post:
+ *     summary: Carga de info de participante global a partir de un pedido de certificado realizado con "/template/request/:requestCode".
+ *     parameters:
+ *       - name: requestCode
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     requestBody:
+ *       required:
+ *         - access_token
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               access_token:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.post(
 	"/:requestCode",
 	Validator.validate([{ name: "access_token", validate: [Constants.VALIDATION_TYPES.IS_STRING] }]),
@@ -304,8 +583,40 @@ router.post(
 );
 
 /**
- *	Carga de info de participante para un template en particular a partir del QR generado en "/template/:id/qr/:requestCode"
+ * @openapi
+ *   /participant/:{templateId}/:{requestCode}:
+ *   post:
+ *     summary: Carga de info de participante para un template en particular a partir del QR generado en "/template/:id/qr/:requestCode".
+ *     parameters:
+ *       - name: templateId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *       - name: requestCode
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type : string
+ *     requestBody:
+ *       required:
+ *         - access_token
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               access_token:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Puede devolver ok o error en algun parametro
+ *       401: 
+ *         description: Acción no autorizada
+ *       500:
+ *         description: Error interno del servidor
  */
+
 router.post(
 	"/:templateId/:requestCode",
 	Validator.validate([{ name: "access_token", validate: [Constants.VALIDATION_TYPES.IS_STRING] }]),
