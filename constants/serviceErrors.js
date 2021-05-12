c96974f24202cbc292a6e198c7fc9bbee4b26c61
Ese <silviaox@gmail.com>
2021-05-12 15:45:17 -0300

    Asegurar calidad de services/MouroService MM-347

--- constants/serviceErrors.js
+++ constants/serviceErrors.js
@@ -7,4 +7,56 @@ module.exports = {
     code: '#service-missingOtherDID',
     message: 'Falta el parámetro OTHERDID.',
   },
+  missingJwt: {
+    code: '#service-missingJwt',
+    message: 'Falta el parámetro JWT.',
+  },
+  missingErrMsg: {
+    code: '#service-missingErrMsg',
+    message: 'Falta el parámetro ERRMSG.',
+  },
+  missingClaims: {
+    code: '#service-missingClaims',
+    message: 'Falta el parámetro CLAIMS.',
+  },
+  missingCb: {
+    code: '#service-missingCb',
+    message: 'Falta el parámetro CB',
+  },
+  missingSubject: {
+    code: '#service-missingSubject',
+    message: 'Falta el parámetro SUBJECT',
+  },
+  missingExpDate: {
+    code: '#service-missingExpDate',
+    message: 'Falta el parámetro EXPDATE',
+  },
+  missingDid: {
+    code: '#service-missingDid',
+    message: 'Fata el parámetro DID',
+  },
+  missingTemplate: {
+    code: '#service-missingTemplate',
+    message: 'Fata el parámetro TEMPLATE',
+  },
+  missingCert: {
+    code: '#service-missingCert',
+    message: 'Fata el parámetro CERT',
+  },
+  missingSendPush: {
+    code: '#service-missingSendPush',
+    message: 'Fata el parámetro SENDPUSH',
+  },
+  missingHash: {
+    code: '#service-missingHash',
+    message: 'Fata el parámetro HASH',
+  },
+  missingSub: {
+    code: '#service-missingSub',
+    message: 'Fata el parámetro SUB',
+  },
+  missingWrapped: {
+    code: '#service-missingWrapped',
+    message: 'Fata el parámetro WRAPPED',
+  }
 };
