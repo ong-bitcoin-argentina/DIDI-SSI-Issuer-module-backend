05d2b2fd6012f298d030a07a7180904959857f28
fedelmar <fsuarez91@gmail.com>
2021-08-25 11:55:52 -0300

    Update RegisterDTO MM-468

--- routes/utils/RegisterDTO.js
+++ routes/utils/RegisterDTO.js
@@ -1,19 +1,24 @@
 /* eslint-disable no-underscore-dangle */
 const { getBlockchainName } = require('./GetBlockchain');
+const { getImageUrl } = require('../../services/utils/imageHandler');
 
-const toDTO = (register) => {
+const toDTO = async (register) => {
   const blockchain = getBlockchainName(register);
+  const imageUrl = await getImageUrl(register.imageId);
 
   return {
     _id: register._id,
     did: register.did,
     name: register.name,
+    description: register.description,
+    imageId: register.imageId,
     expireOn: register.expireOn,
     createdOn: register.createdOn,
     status: register.status,
     messageError: register.messageError,
     blockHash: register.blockHash,
     blockchain,
+    imageUrl,
   };
 };
 
