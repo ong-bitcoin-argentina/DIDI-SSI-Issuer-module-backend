4372a8b0ab4d33e85aac3e9a154c11b2de571466
fedelmar <fsuarez91@gmail.com>
2021-11-30 11:35:58 -0300

    create ShareRequest Model MM-586

--- constants/Messages.js
+++ constants/Messages.js
@@ -59,6 +59,10 @@ module.exports = {
         code: 'INVALID_DID_AND_KEY',
         message: 'Hubo un error al validar el did y la clave privada.',
       },
+      TOKEN: {
+        code: 'TOKEN_ERROR',
+        message: 'Hubo un error al crear del Token de autorización.',
+      },
     },
   },
   DELEGATE: {
@@ -126,6 +130,9 @@ module.exports = {
     ERR: {
       CREATE: { code: 'SHARE_REQ_CREATE', message: 'El pedido de certificados no pudo ser creado.' },
       SEND: { code: 'SHARE_REQ_SEND', message: 'El pedido de certificados no pudo ser enviado.' },
+      NOT_EXIST: { code: 'SHARE_REQ_NOT_EXIST', message: 'No existe el pedido de certificados.' },
+      DELETE: { code: 'SHARE_REQ_DELETE', message: 'El pedido de certificados no pudo ser borrado.' },
+      CERT_TYPES: { code: 'SHARE_REQ_CERT_TYPES', message: 'Una categoria de credencial no es valida' },
     },
   },
   TEMPLATE: {
---
+++ models/dataTypes/ClaimsData.js
@@ -0,0 +1,13 @@
+module.exports = {
+  essential: {
+    type: Boolean,
+  },
+  iss: {
+    type: String,
+    required: true,
+  },
+  reason: {
+    type: String,
+    required: true,
+  },
+};
