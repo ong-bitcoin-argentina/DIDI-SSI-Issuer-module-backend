c9b9aed29cb7303a28cd7301e2bb5a46780bc488
fedelmar <fsuarez91@gmail.com>
2021-11-30 11:16:20 -0300

    create token to authorize issuer MM-586

--- services/TokenService.js
+++ services/TokenService.js
@@ -2,9 +2,12 @@
 const jwt = require('jsonwebtoken');
 const Constants = require('../constants/Constants');
 const Messages = require('../constants/Messages');
+const Register = require('../models/Register');
+const { createJWT } = require('./BlockchainService');
 const {
   missingUserId,
   missingToken,
+  missingDid,
 } = require('../constants/serviceErrors');
 
 const now = function now() {
@@ -31,4 +34,23 @@ module.exports.getTokenData = function getTokenData(token) {
     };
   }
   throw Error(Messages.VALIDATION.INVALID_TOKEN);
+};
+
+/*
+* Crea un token de autorizacion en didi-server, dado un emisor
+*/
+module.exports.createIssuerAuthToken = async (did) => {
+  if (did) throw missingDid;
+  try {
+    const issuer = await Register.getByDid(did);
+    if (!issuer) throw Messages.REGISTER.ERR.NOT_EXIST;
+
+    const payload = { auth: 'Token de autorizacion en didi server' };
+    const token = await createJWT(did, issuer.privateKey, payload);
+    return token;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    throw Error(Messages.REGISTER.ERR.TOKEN);
+  }
 };
