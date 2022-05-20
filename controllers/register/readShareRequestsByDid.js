43af207a78d93adbfb4c222bb0b779f00eacc8ff
SantiagoMaresca <santiago.maresca26@gmail.com>
2022-05-20 16:02:36 -0300

    Added decoded jwt in share request list

--- controllers/register/readShareRequestsByDid.js
+++ controllers/register/readShareRequestsByDid.js
@@ -1,14 +1,21 @@
 const ResponseHandler = require('../../routes/utils/ResponseHandler');
 const RegisterService = require('../../services/RegisterService');
+const BlockchainService = require('../../services/BlockchainService');
 
 const readShareRequestsByDid = async (req, res) => {
   try {
     const { did } = req.params;
 
-    const shareRequestList = await RegisterService.getShareRequestsByDid(did, {
+    const { list, totalPages } = await RegisterService.getShareRequestsByDid(did, {
       iss: did,
     });
-    return ResponseHandler.sendRes(res, shareRequestList);
+    const decodedShareRequestList = await Promise.all(
+      list.map(async (shareReq) => {
+        const { payload } = await BlockchainService.decodeJWT(shareReq.jwt);
+        return { ...shareReq, payload };
+      }),
+    );
+    return ResponseHandler.sendRes(res, { list: decodedShareRequestList, totalPages });
   } catch (err) {
     // eslint-disable-next-line no-console
     console.log(err);
